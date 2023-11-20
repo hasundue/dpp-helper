d6ff9bc7c108cb5c7b7a648b6373d833ed5ad456
hasundue <hasundue@gmail.com>
2023-11-20 13:34:35 +0900

    chore(env): remove neovim-specific paths

--- lib/env.ts
+++ lib/env.ts
@@ -1,5 +1,3 @@
-import { join } from "./std/path.ts";
-
 /** @example /home/username */
 export const $HOME = Deno.env.get("HOME")!;
 
@@ -11,12 +9,3 @@ export const $XDG_CONFIG_HOME = Deno.env.get("XDG_CONFIG_HOME")!;
 
 /** @example ~/.local/share */
 export const $XDG_DATA_HOME = Deno.env.get("XDG_DATA_HOME")!;
-
-/** @example ~/.cache/dpp */
-export const $CACHE = join($XDG_CACHE_HOME, "dpp");
-
-/** @example ~/.config/nvim */
-export const $CONFIG = join($XDG_CONFIG_HOME, "nvim");
-
-/** @example ~/.local/share/nvim */
-export const $DATA = join($XDG_DATA_HOME, "nvim");
--- lib/std/path.ts
+++ lib/std/path.ts
@@ -1,3 +1,2 @@
 export { basename } from "https://deno.land/std@0.207.0/path/basename.ts";
 export { extname } from "https://deno.land/std@0.207.0/path/extname.ts";
-export { join } from "https://deno.land/std@0.207.0/path/join.ts";
