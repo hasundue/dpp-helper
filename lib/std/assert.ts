9c34595c4f8c27b7bf144f236bdbbc1586da93b3
hasundue <hasundue@gmail.com>
2023-12-02 17:31:18 +0900

    build(deps): bump deno.land/std from 0.207.0 to 0.208.0

--- lib/std/assert.ts
+++ lib/std/assert.ts
@@ -1,2 +1,2 @@
-export { assertEquals } from "https://deno.land/std@0.207.0/assert/assert_equals.ts";
-export { assertObjectMatch } from "https://deno.land/std@0.207.0/assert/assert_object_match.ts";
+export { assertEquals } from "https://deno.land/std@0.208.0/assert/assert_equals.ts";
+export { assertObjectMatch } from "https://deno.land/std@0.208.0/assert/assert_object_match.ts";
